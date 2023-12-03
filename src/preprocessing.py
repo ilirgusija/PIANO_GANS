0ba337181f0e6085153ae8647e9aa037c674aa78
Ilir Gusija <ilirgusija@gmail.com>
2023-12-03 14:42:58 -0500

    updated code

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -11,9 +11,9 @@ import skimage.io
 import skimage.transform
 from pydub import AudioSegment
 from pydub.utils import make_chunks
+import json
 
 import preprocessing_utils as prep_utils
-
 
 DATASET_DIR = "../data/audio/maestro-v3.0.0"
 AUDIO_CHUNKS_10S_DIR = "../data/audio/audio_chunks_10s/"
@@ -23,17 +23,6 @@ AUDIO_OUT_DIR = "../output/"
 STFT_ARRAY_DIR = "../data/stft_arrays/"
 PROCESSED_STFT_DIR = "../data/clipped_stft/"
 RESIZED_STFT_DIR = "../data/resized_stft/"
-
-STYLEGAN_STFT_IMAGES_256_DIR = "../data/styleGAN/stft_images_256/"
-STYLEGAN_STFT_IMAGES_512_GRAY_DIR = "../data/styleGAN/stft_images_512_gray/"
-STYLEGAN_STFT_ARRAYS_DIR = "../data/styleGAN/stft_arrays/"
-STYLEGAN_STFT_IMAGES_TEST_DIR = "../data/styleGAN/test_images/"
-
-STYLEGAN_AUDIO_TEST_DIR = "../output/styleGAN/styleGAN_preprocess_test/"
-STYLEGAN_AUDIO_OUTPUT_DIR = "../output/styleGAN/audio/"
-STYLEGAN_STFT_IMAGES_FAKE_DIR = "../output/styleGAN/stft_images_fake/"
-STYLEGAN_STFT_IMAGES_FAKE_TEMP_DIR = "../output/styleGAN/stft_images_fake_temp/"
-
 
 def make_audio_chunks(seconds, dest_dir):
     """
@@ -45,7 +34,7 @@ def make_audio_chunks(seconds, dest_dir):
     paths = prep_utils.get_absolute_file_paths(DATASET_DIR, ".wav")
 
     start_time = time.time()
-    for audio_path in paths:audio_reconstruction
+    for audio_path in paths:
         prep_utils.display_progress_eta(current_item=audio_path, total_items=paths, start_time=start_time)
 
         audio = AudioSegment.from_file(audio_path)
@@ -119,7 +108,9 @@ def audio_reconstruction():
     """
     paths = prep_utils.get_absolute_file_paths(STFT_ARRAY_DIR)
 
+    start_time = time.time()
     for path in paths:
+        prep_utils.display_progress_eta(current_item=path, total_items=paths, start_time=start_time)
         S = np.load(path)
         y = librosa.griffinlim(S)
 
@@ -127,6 +118,54 @@ def audio_reconstruction():
 
         # Save reconstructed data
         scipy.io.wavfile.write(out, 22050, y)
+
+def create_cache_file(cache_file_path, hop_length, input_freq, input_time, eps=1e-7):
+    """
+    Creates a cache file containing mean and std of STFT magnitudes, hop length, input frequency, input time, 
+    epsilon value, and sampling rate for each audio file.
+
+    :param audio_paths: List of paths to the audio files.
+    :param cache_file_path: Path to save the cache file.
+    :param hop_length: Hop length used in the STFT.
+    :param input_freq: Frequency dimension for resizing the STFT.
+    :param input_time: Time dimension for resizing the STFT.
+    :param eps: Small constant to prevent division by zero.
+    """
+    cache_data = {}
+
+    paths = prep_utils.get_absolute_file_paths(STFT_ARRAY_DIR)
+
+    start_time = time.time()
+    for path in paths:
+        prep_utils.display_progress_eta(current_item=path, total_items=paths, start_time=start_time)
+        # Compute mean and std of magnitude
+        try:
+            S = np.load(path)
+        except Exception as e:
+                print(f"Error loading file {path}: {e}")
+                continue
+        S = np.log(S)
+        mag_mean = float(np.mean(S))
+        mag_std = float(np.std(S))
+        sr = 22050
+
+        # Save data in cache
+        cache_data[path] = {
+            'Mean Magnitude': mag_mean,
+            'Std Magnitude': mag_std,
+            'hop_length': hop_length,
+            'input_freq': input_freq,
+            'input_time': input_time,
+            'eps': eps,
+            'sampling_rate': sr
+        }
+
+    cache_file_path = os.path.join(cache_file_path, "my_cache.json")
+    # Write cache data to JSON file
+    with open(cache_file_path, 'w') as fp:
+        json.dump(cache_data, fp, indent=4)
+
+    print(f"Cache file created at {cache_file_path}")
 
 def record_mean_std():
     """
@@ -137,8 +176,14 @@ def record_mean_std():
     mean_list = []
     std_list = []
 
+    start_time = time.time()
     for path in paths:
-        S = np.load(path)
+        prep_utils.display_progress_eta(current_item=path, total_items=paths, start_time=start_time)
+        try:
+            S = np.load(path)
+        except Exception as e:
+                print(f"Error loading file {path}: {e}")
+                continue
         S = np.log(S)
         mag_mean = np.mean(S)
         mag_std = np.std(S)
@@ -181,9 +226,11 @@ def downsample():
     Downsample and resize to 256x256, and save them locally
     """
     paths = prep_utils.get_absolute_file_paths(PROCESSED_STFT_DIR)
+    start_time = time.time()
     for path in paths:
+        prep_utils.display_progress_eta(current_item=path, total_items=paths, start_time=start_time)
         S = np.load(path)
-        S_downsample = skimage.transform.resize(S, (256, 256), anti_aliasing=True)
+        S_downsample = skimage.transform.resize(S, (512, 512), anti_aliasing=True)
         out = RESIZED_STFT_DIR + prep_utils.get_filename(path) + ".npy"
         np.save(out, S_downsample)
 
@@ -233,74 +280,14 @@ def normalize_stft(s):
     s /= 3
     return s
 
-def convert_stft_to_images_grayscale(src_dir, dest_dir, ext=".png", size=None):
-    """
-    Convert STFT matrices into grayscale images
-
-    :param src_dir: source directory that stores STFT matrices
-    :param dest_dir: destination where converted images are being saved
-    :param ext: image extension
-    :param size: resize dimension
-    """
-    paths = prep_utils.get_unprocessed_items(src_dir=src_dir, dest_dir=dest_dir)
-
-    start_time = time.time()
-    for path in paths:
-        prep_utils.display_progress_eta(current_item=path, total_items=paths, start_time=start_time)
-
-        S = np.load(path)
-        S_scaled = prep_utils.increase_brightness(S)
-
-        if size:
-            S_scaled = cv2.resize(S_scaled, (size, size), interpolation=cv2.INTER_CUBIC)
-
-        out_path = dest_dir + prep_utils.get_filename(path) + ext
-        cv2.imwrite(out_path, S_scaled)
-
-def dc_gan_processing():
-    paths = []
-    for subdir, dirs, files in os.walk(directory):
-        for file in files:
-            if file.lower().endswith(extension):
-                path = os.path.join(subdir, file)
-                paths.append(path)
-    
-    for path in paths:
-        print("Converting ", path)
-
-        y, sr = librosa.load(path, sr=10000)
-
-        # Acquire magnitude matrix
-        D = librosa.stft(y, n_fft=1024, hop_length=256)
-        S, phase = librosa.magphase(D)  # S shape (513, 782)
-
-        # normalize S and downsample
-        S = normalize_stft(S)
-        S = cv2.resize(S, (512, 512), interpolation=cv2.INTER_AREA)
-
-        _, file_name = os.path.split(path)
-        out = STFT_ARRAY_DIR + os.path.splitext(file_name)[0] + ".npy"
-
-        np.save(out, S)
-
-def style_gan_preprocessing():
-    """
-    Data processing for StyleGAN2-ADA-Pytorch
-    """
-    make_audio_chunks(seconds=10, dest_dir=AUDIO_CHUNKS_10S_DIR)
-    convert_audio_to_stft(src_dir=AUDIO_CHUNKS_10S_DIR, dest_dir=STYLEGAN_STFT_ARRAYS_DIR, extension=".npy")
-    convert_stft_to_images(src_dir=STYLEGAN_STFT_ARRAYS_DIR, dest_dir=STYLEGAN_STFT_IMAGES_TEST_DIR, size=256)
-    convert_stft_to_images_grayscale(src_dir=STYLEGAN_STFT_ARRAYS_DIR, dest_dir=STYLEGAN_STFT_IMAGES_512_GRAY_DIR,
-                                     size=512)
-
 def preprocessing():
     """
     Data processing for DCGAN and SpecGAN
     """
     make_audio_chunks(seconds=20, dest_dir=AUDIO_CHUNKS_20S_DIR)
-    display_spectrogram()
-    convert_audio_to_stft(src_dir=AUDIO_CHUNKS_10S_DIR, dest_dir=STFT_ARRAY_DIR, extension=".npy")
-    audio_reconstruction()
+    # display_spectrogram()
+    convert_audio_to_stft(src_dir=AUDIO_CHUNKS_20S_DIR, dest_dir=STFT_ARRAY_DIR, extension=".npy")
+    # audio_reconstruction()
     record_mean_std()
     preprocessing_arrays()
     downsample()
@@ -308,5 +295,6 @@ def preprocessing():
 
 if __name__ == "__main__":
     preprocessing()
-    dc_gan_processing()
-    style_gan_preprocessing()
+    create_cache_file(STFT_ARRAY_DIR, 512, 1025, 862)
+    # dc_gan_processing()
+    # style_gan_preprocessing()
