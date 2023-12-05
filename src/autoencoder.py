7468002ea8b96ef744c5fdb7794a13693e4a67f2
damiensmith1 <damiensmith@damiens-air.home>
2023-12-05 14:24:29 -0500

    small fix

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -6,14 +6,6 @@ from torch.utils.data import DataLoader
 from model import Generator
 from CustomDataset import CustomDataset
 
-# Define the autoencoder model
-class Autoencoder(nn.Module):
-    def __init__(self, generator):
-        super(Autoencoder, self).__init__()
-        self.generator = generator
-
-    def forward(self, x):
-        return self.generator(x)
 
 # Set your hyperparameters
 LATENT_DIM = 100
@@ -23,23 +15,21 @@ EPOCHS = 10
 
 # Initialize the generator and autoencoder
 generator = Generator()
-autoencoder = Autoencoder(generator)
 
 STFT_ARRAY_DIR = "../data/resized_stft/"
 
 # Define loss function and optimizer
 criterion = nn.MSELoss()
-optimizer = optim.Adam(autoencoder.parameters(), lr=LEARNING_RATE)
+optimizer = optim.Adam(generator.parameters(), lr=LEARNING_RATE)
 
-train_set = CustomDataset(data_dir=STFT_ARRAY_DIR)
-train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
+data_set = CustomDataset(data_dir=STFT_ARRAY_DIR)
+data_loader = DataLoader(data_set, batch_size=64, shuffle=True)
 
 # Pretrain the autoencoder
 for epoch in range(EPOCHS):
     running_loss = 0.0
-    for data in train_loader:
-        # Assuming data is a batch of images, no need for labels in autoencoder training
-        images, _ = data
+    for i, data in enumerate(data_loader):
+        images = data
         
         # Flatten the images and generate random noise as input
         images = images.view(images.size(0), -1)
@@ -49,7 +39,7 @@ for epoch in range(EPOCHS):
         optimizer.zero_grad()
         
         # Forward pass
-        outputs = autoencoder(noise)
+        outputs = generator(noise)
         
         # Compute the loss
         loss = criterion(outputs, images)
@@ -60,6 +50,6 @@ for epoch in range(EPOCHS):
         
         running_loss += loss.item()
     
-    print(f"Epoch [{epoch + 1}/{EPOCHS}], Loss: {running_loss / len(train_loader)}")
+    print(f"Epoch [{epoch + 1}/{EPOCHS}], Loss: {running_loss / len(data_loader)}")
 
-torch.save(autoencoder.state_dict(), 'autoencoder_model.pth')
+torch.save(generator.state_dict(), 'autoencoder_model.pth')
